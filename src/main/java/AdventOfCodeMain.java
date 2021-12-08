c5e8ee84e23ce67174bf4177690ecf565c98dff6
Vlets <georgevle93@gmail.com>
2021-12-08 11:08:56 +0100

    Day 1 Part 1

--- src/main/java/AdventOfCodeMain.java
+++ src/main/java/AdventOfCodeMain.java
@@ -2,6 +2,8 @@ import helpers.FileHelper;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static helpers.Constants.FILE_NAME;
@@ -10,12 +12,28 @@ public class AdventOfCodeMain {
 
     public static void main(String[] args) throws FileNotFoundException {
         File inputFile = FileHelper.readFile(FILE_NAME);
-        Scanner scanner;
+        Scanner scanner = null;
         if (inputFile.exists()) {
             scanner = FileHelper.scanFile(inputFile);
             assert scanner != null;
         }
+        List<Integer> inputList = new ArrayList<Integer>();
 
+        while(scanner.hasNext()) {
+            inputList.add(Integer.parseInt(scanner.next()));
+        }
 
+        System.out.println("Part one answer: " + calculatePartOneAnswerOne(inputList));
+
+    }
+
+     private static Integer calculatePartOneAnswerOne(final List<Integer> partOneInput) {
+        Integer resultCounter = 0;
+         for (int i = 0; i < partOneInput.size()-1; i ++) {
+            if (partOneInput.get(i)<partOneInput.get(i+1)){
+                resultCounter++;
+            }
+         }
+        return resultCounter;
     }
 }
